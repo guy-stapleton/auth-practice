12c79164eafd715195a365cfe0044c5026f99f25
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-06-03 20:46:06 +1200

    Linked in auth routes

--- server/server.js
+++ server/server.js
@@ -3,9 +3,13 @@ const bodyParser = require('body-parser')
 
 const app = express()
 
+const authRoutes = require('./routes/auth')
+
 app.use(express.static('public'))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: false }))
+
+app.use('/api/auth', authRoutes)
 
 app.get('/msg', (req, res) => {
   res.json({ "greeting": "Hello World!" })
