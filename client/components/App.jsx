2985fd5f248add59a8d007c69901e3918ad103b7
Guy Stapleton <guy.stapleton+1@gmail.com>
2018-06-03 17:13:26 +1200

    Set up basic App component

---
+++ client/components/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+  }
+  render() {
+    return(
+      <div>
+        <h1>Welcome to the authenticator</h1>
+      </div>
+    )
+  }
+}
+
+export default App
